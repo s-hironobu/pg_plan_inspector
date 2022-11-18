5ec58e9150247524e070bda7d8456e7b2856989b
suzuki hironobu <hironobu@interdb.jp>
2022-11-18 12:46:04 +0900

    Support PG15

--- src/pg_query_plan.c
+++ src/pg_query_plan.c
@@ -105,6 +105,9 @@ static int	pgqp_hash_max;
 static volatile sig_atomic_t got_siguser2 = false;
 
 /* Saved hook values in case of unload */
+#if PG_VERSION_NUM >= 150000
+static shmem_request_hook_type prev_shmem_request_hook = NULL;
+#endif
 static shmem_startup_hook_type prev_shmem_startup_hook = NULL;
 #if PG_VERSION_NUM < 140000
 static post_parse_analyze_hook_type prev_post_parse_analyze_hook = NULL;
@@ -139,6 +142,9 @@ static void pgqp_ProcessUtility(PlannedStmt *pstmt, const char *queryString,
 								ParamListInfo params, QueryEnvironment *queryEnv,
 								DestReceiver *dest, QueryCompletion *qc);
 
+#if PG_VERSION_NUM >= 150000
+static void pgqp_shmem_request(void);
+#endif
 static void pgqp_shmem_startup(void);
 static void pgqp_shmem_shutdown(int code, Datum arg);
 static Size pgqp_memsize(void);
@@ -246,12 +252,20 @@ _PG_init(void)
 							 NULL);
 #endif
 
+#if PG_VERSION_NUM >= 150000
+	MarkGUCPrefixReserved("pg_query_plan");
+#else
 	EmitWarningsOnPlaceholders("pg_query_plan");
 
 	RequestAddinShmemSpace(pgqp_memsize());
 	RequestNamedLWLockTranche("pg_query_plan", 1);
+#endif
 
 	/* Install hooks. */
+#if PG_VERSION_NUM >= 150000
+	prev_shmem_request_hook = shmem_request_hook;
+	shmem_request_hook = pgqp_shmem_request;
+#endif
 	prev_shmem_startup_hook = shmem_startup_hook;
 	shmem_startup_hook = pgqp_shmem_startup;
 
@@ -310,6 +324,17 @@ _PG_init(void)
 #endif
 }
 
+#if PG_VERSION_NUM >= 150000
+static void
+pgqp_shmem_request(void)
+{
+	if (prev_shmem_request_hook)
+		prev_shmem_request_hook();
+
+	RequestAddinShmemSpace(pgqp_memsize());
+	RequestNamedLWLockTranche("pg_query_plan", 1);
+}
+#endif
 
 void
 _PG_fini(void)
